9363dcfce6a6d1fe4ffc8603537a13465b7fc4f3
Sergei <Sergei@DESKTOP-7BFDA>
2023-04-19 18:18:24 +0300

    Add project files.

---
+++ StartClass/Start.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartClass
+{
+    class Startclass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("sissestage esimene arv");
+            int arv1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("sissestage teine arv");
+            int arv2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Arvude {0} ja {1} korutis võrdub {2}",arv1,arv2, arv1 * arv2);
+        }
+    }
+    
+}
